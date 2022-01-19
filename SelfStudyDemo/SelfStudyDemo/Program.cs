5397076d7ac09c2caa9f272576d9269b4ae13672
Wenling Lyu <wlyu1@nait.ca>
2022-01-18 17:46:18 -0700

    enum

---
+++ SelfStudyDemo/SelfStudyDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using 枚举; // using namespace
+using static System.Console; // no more console, just WirteLine() or ReadLine()
+
+WriteLine("How dare you");
+
+// enum 的使用类似其他data type的使用
+
+Gender Burger= Gender.Male;
+WriteLine($"Burger's gender is {Burger}");
+
+Seasons myFav = Seasons.Summer;
+
+QQstate myState = QQstate.Busy;
---
+++ SelfStudyDemo/SelfStudyDemo/enum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 枚举
+{
+
+    // 此处声明了一个enum
+    public enum Gender
+    {
+        Male,
+        Female // Without , means its the last value of Gender
+    }
+
+    public enum Seasons
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+    }
+
+    public enum QQstate
+    {
+        Online,
+        Offline,
+        Leave,
+        Busy,
+        QMe
+    }
+    internal class @enum
+    {
+    }
+}
