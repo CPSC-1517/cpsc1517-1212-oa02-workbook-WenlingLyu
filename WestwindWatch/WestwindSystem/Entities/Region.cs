51a4b6cf419cfd70f6433ab670a3e77a030a08ce
Wenling Lyu <wlyu1@nait.ca>
2022-03-04 10:58:51 -0700

    03-04 insert database

---
+++ WestwindWatch/WestwindSystem/Entities/Region.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations; //for Key, Required, StringLength
+using System.ComponentModel.DataAnnotations.Schema;//for Table
+
+
+namespace WestwindSystem.Entities
+{
+    [Table("Regions")] //Data table
+    public class Region
+    {
+        [Key] // Primary key
+        public int RegionId { get; set; }   
+        [Required(ErrorMessage ="Region Description is required")]
+        [StringLength(50,ErrorMessage ="Region Description is limited 50 characters")]
+        public string RegionDescription { get; set; }
+    }
+}
