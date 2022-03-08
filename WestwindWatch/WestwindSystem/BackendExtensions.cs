695146cae6ed2682215c23d38852cb788b3ca093
Wenling Lyu <wlyu1@nait.ca>
2022-03-08 14:52:15 -0700

    03-08 class

---
+++ WestwindWatch/WestwindSystem/BackendExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;//for dbContextOptionBuilder
+using Microsoft.Extensions.DependencyInjection; //for IserviceCollection
+namespace WestwindSystem
+{
+    public static class BackendExtensions
+    {
+        public static void WestwindBackendDependencies(
+            this IServiceCollection service,
+            Action<DbContextOptionsBuilder>option)
+        {
+
+        }
+    }
+}
---
+++ WestwindWatch/WestwindSystem/Entities/Category.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations; // for [Key],[Required],[StringLength]
+using System.ComponentModel.DataAnnotations.Schema; // for [Table] 
+
+
+namespace WestwindSystem.Entities
+{
+    [Table("Categories")]
+
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+
+        [Required(ErrorMessage ="Category name is required.")]
+        [StringLength(15,ErrorMessage ="Category name must contain 15 or less characters.")]
+        [Column("CategoryName")] // use this to match Name with dataBase name "categoryName"
+        //if dont use this, it could not find what name is cuz it not exist in database
+        public string Name { get; set; } = null!;
+        // = null! means if this property is null then it should assign a value,
+        // in this case , it means it assign a null to this property
+        [Column(TypeName ="next")]
+        public string? Description { get; set; } // ?  means this property is nullable
+
+    }
+}
